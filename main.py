b5cb22952141614f2a2ad3e79f7556d89061a578
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2024-05-25 20:24:54 +0800

    flask server done

--- main.py
+++ main.py
@@ -7,32 +7,30 @@ import os
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["POST", "GET"])
 def search():
-    if request.method == "POST": # if sending via post, receiving a link from user
-        textInput = request.form.get("search_query")
-        #language = request.form.get("language")
-        
-        print(textInput)
-        #print(language)
+ # if sending via post, receiving a link from user
+    textInput = request.get_json()["search_query"]
+    #language = request.form.get("language")
+    
+    print(textInput)
+    #print(language)
 
-        text = main(textInput)
-        summary = summarise(''.join(text))
-        translatedText = translate(summary, "en", "ch")
+    text = main(textInput)
+    summary = summarise(''.join(text))
+    translatedText = translate(summary, "en", "ch")
 
-        print(summary)
-        print(translatedText)
+    print(summary)
+    print(translatedText)
 
-        ensure_audio_directory()
+    ensure_audio_directory()
 
-        generate_audio(summary, 'en', 'uk')
-        generate_audio(summary, 'en', 'us')
-        generate_audio(translatedText, 'zh-CN', 'cn')
-        generate_audio(translatedText, 'zh-TW', 'tw')
-
-        return send_file("\\audio\\gtts_en_uk.mp3", as_attachment=True)
-    else: #if arrived at this page by themselves, is going to search
-        return 0
+    generate_audio(summary, 'en', 'uk')
+    generate_audio(summary, 'en', 'us')
+    generate_audio(translatedText, 'zh-CN', 'cn')
+    generate_audio(translatedText, 'zh-TW', 'tw')
+    print("HELLO")
+    return send_file("audio2\\gtts_en_uk.mp3", as_attachment=True)
 
 if __name__ == '__main__':  
    app.run()
--- test.py
+++ test.py
@@ -7,7 +7,14 @@ load_dotenv()
 textInput = input()
 payload = {"search_query": textInput}
 
-response = requests.post('http://127.0.0.1:5000', json=payload)
+response = requests.get('http://127.0.0.1:5000', json=payload, stream=True)
+with open("returnedVideo.mp3", 'wb') as f:
+    for chunk in response.iter_content(chunk_size=8192): 
+        # If you have chunk encoded response uncomment if
+        # and set chunk_size parameter to None.
+        #if chunk: 
+        f.write(chunk)
+
 # Check if the request was successful
 if response.status_code == 200: # Assuming the response is in JSON format
     print("success!")
