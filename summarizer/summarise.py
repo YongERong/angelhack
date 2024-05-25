a644aa2fa5afafd1fa3a895165fa4ca3618f35d9
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2024-05-25 15:25:02 +0800

    summarise and translate

---
+++ summarizer/summarise.py
@@ -0,0 +1,38 @@
+import requests 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def summarise(text): 
+    payload = {'text': text}
+    headers = {'x-api-key': os.getenv("jigsaw_key")}
+
+    response = requests.post('https://api.jigsawstack.com/v1/ai/summary', json=payload, headers=headers)
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()["summary"]  # Assuming the response is in JSON format
+        print(data)
+        return data
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+        return 0
+
+def translate(text, currLang, targetLang): 
+    payload = {'text': text, "current_language": currLang, "target_language": targetLang}
+    headers = {'x-api-key': os.getenv("jigsaw_key")}
+
+    response = requests.post('https://api.jigsawstack.com/v1/ai/translate', json=payload, headers=headers)
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()["translated_text"]  # Assuming the response is in JSON format
+        print(data)
+        return data
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+        return 0
+
+
+file = open("newsarticle.txt", "r").read()
+summarisedText = summarise(file)
+translate(summarisedText, "en", "ch")
